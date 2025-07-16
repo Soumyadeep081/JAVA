b39b50e38807a226878fe0b5d12a117ad8a41491
Soumyadeep081 <soumyadeep323@gmail.com>
2025-07-16 23:44:00 +0530

    Second Program

---
+++ src/Day1/PrintFunction.java
@@ -0,0 +1,13 @@
+package Day1;
+
+public class PrintFunction {
+    public static void main(String[] args) {
+          System.out.print("hello ");
+          System.out.print("there");
+          System.out.println();
+          System.out.printf("hello %s!%n","world");
+         System.out.printf("JAVA is %d percent interesting",100);
+        System.out.println();
+         System.out.printf("JAVA is %s percent interesting","100");
+    }
+}
