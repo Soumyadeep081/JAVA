9d5197a859802d672278faa03bcf6d788cf1baa3
Soumyadeep081 <soumyadeep323@gmail.com>
2025-07-16 23:48:18 +0530

    Third Program

---
+++ src/Day1/Variables.java
@@ -0,0 +1,21 @@
+package Day1;
+
+public class Variables {
+    public static void main(String[] args) {
+        //  Variable is a location in memory to hold data. to indicate storage area
+         int a = 10;
+         int b=30;
+         int c;
+         c=(a+b);
+         System.out.printf("Result = %d\n",c);
+         int speed=10;
+         System.out.printf("the speed is=%d\n",speed);
+         boolean flag=true;
+         System.out.println("Flag= "+flag);
+         byte range;
+         range=12+3;
+         System.out.println("Range"+range);
+         float value=12.5f;
+        System.out.println("Value= "+value);
+    }
+}
