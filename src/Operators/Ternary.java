0c0b659126b6f176527ae7d09fac15ad025dfd66
Soumyadeep081 <soumyadeep323@gmail.com>
2025-07-17 15:06:54 +0530

    New Commit

---
+++ src/Operators/Ternary.java
@@ -0,0 +1,18 @@
+package Operators;
+
+public class Ternary {
+    public static void main(String[] args) {
+
+//Ternary operators:-
+//Used to skip if else statements.
+//Syntax: condition?statement1:statement2;
+        //True     //False
+
+ int marks=76;
+
+ String result = marks>75?"true":"false";
+ System.out.println(result);
+
+
+    }
+}
