a37d320bdb6a2ef327b2626db5b72d04f06cafd3
Soumyadeep081 <soumyadeep323@gmail.com>
2025-08-08 00:09:06 +0530

    new commit

---
+++ src/TwoPointers/Q6.java
@@ -0,0 +1,20 @@
+package TwoPointers;
+
+import java.util.Arrays;
+
+public class Q6 {
+    public static void main(String[] args) {
+        //Move Zeroes:
+        int[]a={0,1,0,2,0,3};
+        int i=0;
+        for(int j=0;j<a.length;j++){
+            if(a[j]!=0){
+                a[i++]=a[j];
+            }
+        }
+        for(int k=i;k<a.length;k++){
+            a[k]=0;
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}
