5c16abf7ab30c785a12a7fb8f492f815a8d019f9
Soumyadeep081 <soumyadeep323@gmail.com>
2025-08-11 00:57:11 +0530

    New commit

---
+++ src/TwoPointers_Or_SlidingWindow/Q14.java
@@ -0,0 +1,23 @@
+package TwoPointers_Or_SlidingWindow;
+
+import java.util.HashMap;
+import java.util.stream.Gatherer;
+
+public class Q14 {
+    public static void main(String[] args) {
+        //Fruits into baskets:
+        int[] f={0,1,2,2};
+        int left=0,max=0;
+        HashMap<Integer,Integer>hm=new HashMap<>();
+        for(int right=0;right<f.length;right++){
+            hm.put(f[right],hm.getOrDefault(f[right],0)+1);
+            while(hm.size()>2){
+                hm.put(f[left],hm.get(f[left])-1);
+                if(hm.get(f[left])==0) hm.remove(f[left]);
+                left++;
+            }
+            max=Math.max(max,right-left+1);
+        }
+        System.out.println(max);
+    }
+}
