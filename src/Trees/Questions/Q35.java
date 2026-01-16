e096629400765abda1c6d0a1237ae93a65f08457
Soumyadeep081 <soumyadeep323@gmail.com>
2026-01-16 20:08:12 +0530

    Tree

---
+++ src/Trees/Questions/Q35.java
@@ -0,0 +1,42 @@
+package Trees.Questions;
+
+public class Q35 {
+    static class Node{
+        int data;
+        Node left,right;
+        public Node(int data){
+            this.data=data;
+            this.left=this.right=null;
+        }
+    }
+    public static void main(String[] args) {
+        //Invert Tree:
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        preorder(root);
+    }
+    public static void preorder(Node root){
+        if(root==null) {
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public static Node invert(Node root){
+        if(root==null)return root;
+        Node temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+
+        invert(root.left);
+        invert(root.right);
+        return root;
+    }
+}
